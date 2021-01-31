bd6969be5b108b789ea3a1cc02b2b75ca68601d3
PoOwAa <raycsucsu@gmail.com>
2021-01-31 23:21:16 +0100

    slow and error response

--- app/src/index.js
+++ app/src/index.js
@@ -51,6 +51,23 @@ app.get('/user/:id', function (req, res) {
   });
 });
 
+app.get('/slow/:time', function (req, res) {
+  const { time } = req.params;
+  setTimeout(() => {
+    res.json({
+      status: 200,
+      message: `I am a very slow endpoint`,
+    });
+  }, time);
+});
+
+app.get('/error', function (req, res) {
+  res.status(500).json({
+    status: 500,
+    message: `I'm an error!`,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
 });
